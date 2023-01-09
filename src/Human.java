184698823a90c5c9b2bfd73960ae73f542969d3a
Nguyen Thanh Luan <luann4099@gmail.com>
2023-01-09 19:08:55 +0700

    update code

---
+++ src/Human.java
@@ -0,0 +1,19 @@
+public class Human {
+    public String name;
+    public int age;
+    public float height;
+    public float weight;
+    public String id;
+
+    public void eat() {
+        System.out.println(this.name+" eating");
+    }
+
+    public void sleep() {
+        System.out.println(this.name+" sleeping");
+    }
+
+    public void walk() {
+        System.out.println(this.name+" walking");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Nghĩa \n \"bóng\" ");
-        //int num = 10;
-        //System.out.println(++num);//10
-        //System.out.println(num);//11
-        /*int a = 20;
-        long b  = a;//chuyển ngầm định
+        Human luan = new Human();
+        luan.name = "Luân";
+        luan.age = 30;
+        luan.walk();
+        luan.eat();
+        luan.sleep();
 
-        int c = 20;
-        short d = (short) c;//chuyển tường minh*/
-
-        System.out.println("Vui lòng nhập vào tuổi");
-        Scanner scanner = new Scanner(System.in);
-        int age = scanner.nextInt();
-        if (age <= 12) {
-            System.out.println("Bạn là trẻ em");
-        } else if (age <= 18) {
-            System.out.println("Bạn là thanh thiếu niên");
-        } else if (age <= 30) {
-            System.out.println("Bạn là thanh niên");
-        } else if (age <= 50) {
-            System.out.println("Bạn là trung niên");
-        } else {
-            System.out.println("Bạn là người già");
-        }
-
-        //switch case
-        switch (age) {
-            case 1:
-                System.out.println("Bạn là trẻ em");
-                break;
-            case 2:
-                System.out.println("Bạn là thanh thiếu niên");
-                break;
-            case 3:
-                System.out.println("Bạn là thanh niên");
-                break;
-            case 4:
-                System.out.println("Bạn là trung niên");
-                break;
-            default:
-                System.out.println("Bạn là người già");
-                break;
-        }
+        Human toan = new Human();
+        toan.name = "Toan";
+        toan.age = 30;
+        toan.walk();
+        toan.eat();
+        toan.sleep();
     }
 }
