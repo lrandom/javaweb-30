b6d9e5260be974a2bf24b6ee18d1fc880a182df9
Nguyen Thanh Luan <luann4099@gmail.com>
2023-02-09 19:06:46 +0700

    session 4 + 5 một tí

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
+
+
+import models.*;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Human luan = new Human("Luân",30,1.7f,72,"123456789");
+        Scanner scanner = new Scanner(System.in);
+        Human luan = new Human("Luân", 30, 1.7f, 72, "123456789");
         luan.walk();
         luan.eat();
         luan.sleep();
 
-        Human toan = new Human("Toàn",30,1.7f,72,"123456789");
+        Human toan = new Human("Toàn", 30, 1.7f, 72, "123456789");
         toan.walk();
         toan.eat();
         toan.sleep();
 
         System.out.println(Human.className);
         System.out.println(Human.getClassName());
+
+        models.Human myMan = new models.Human();
+        Animal animal = new Animal();
     }
 }
